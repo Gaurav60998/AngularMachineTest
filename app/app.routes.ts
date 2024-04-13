34a3662e16a7b0314df927b22e671490a84a7895
Gaurav60998 <gdakare06@gmail.com>
2024-04-14 00:41:46 +0530

    new

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -2,13 +2,12 @@ import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { RigisterFormComponent } from './components/rigister-form/rigister-form.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
-import { AboutComponent } from './components/about/about.component';
+
 
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
     { path: 'home', component: HomepageComponent },
     { path: 'rigister', component: RigisterFormComponent },
-    { path: 'User', component: UserProfileComponent },
-    { path: 'about',component:AboutComponent}
+    { path: 'User', component: UserProfileComponent }
 ];
